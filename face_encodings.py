cf2ac317f9a4cb33622ff97d1ed4d5d4be404dd5
Nishant  Mete <nishumete17@gmail.com>
2024-03-21 08:57:00 +0000

    Finished testing

--- face_encodings.py
+++ face_encodings.py
@@ -10,13 +10,13 @@ def euclidean_distance(encoding1, encoding2):
 # calculate the encodings of the faces in an image 
 # and then compare it 
 
-image_path = "comparison/dhoni_7.jpg"
+image_path = "comparison/dhoni_1.jpg"
 face_image = face_recognition.load_image_file(image_path)
 face_locations = face_recognition.face_locations(face_image, model='cnn')
 known_face_locations = [face_locations]  # Convert to a list of lists
 encodings = face_recognition.face_encodings(face_image)
 
-image_path_2 = "comparison/dhoni_7.jpg"
+image_path_2 = "comparison/dhoni_11.jpg"
 face_image_2 = face_recognition.load_image_file(image_path_2)
 face_locations_2 = face_recognition.face_locations(face_image_2, model='cnn')
 known_face_locations_2 = [face_locations_2]  # Convert to a list of lists
@@ -29,7 +29,14 @@ if euclidean_distances[0] < 0.6:
 else:
     print("Faces do not match!")
 
-if encodings[0].all() == encodings_2[0].all():
-    print("Encoding values match too!")
+
+encodings = np.array(encodings)  # Convert list to NumPy array
+encodings_2 = np.array(encodings_2)  # Convert list to NumPy array
+results = face_recognition.compare_faces([encodings], encodings_2) 
+
+if results[0].any() == True:
+    print("The faces match!")
+else:
+    print("The faces do not match.")
 
 
